b67fd23c722d509d15aebf6ed98f7f8dfa455503
Ken Yourek <ken@yourek.net>
2023-10-13 07:59:31 -0500

    Change the console color according to log severity.

---
+++ sln/Domore.Logs.Sample/Logs/Sample.cs
@@ -0,0 +1,24 @@
+﻿using Domore.Conf.Logs;
+using System;
+using CONF = Domore.Conf.Conf;
+
+namespace Domore.Logs {
+    internal sealed class Sample {
+        private static void Main() {
+            CONF.Contain(@"
+                log[console].type                    = console
+                log[console].config.default.severity = debug
+                log[console].config.default.format   = {log} ({sev})
+            ").ConfigureLogging();
+
+            var log = Logging.For(typeof(Sample));
+            if (log.Debug()) log.Debug($"Now it's {DateTime.Now}.");
+            if (log.Info()) log.Info($"This is the logging sample.");
+            if (log.Warn()) log.Warn($"Hey! Look out!");
+            if (log.Error()) log.Error($"too late...");
+            if (log.Critical()) log.Critical($"We've got to exit.");
+
+            Logging.Complete();
+        }
+    }
+}
--- sln/Domore.Logs/Logs/Service/ConsoleLog.cs
+++ sln/Domore.Logs/Logs/Service/ConsoleLog.cs
@@ -1,9 +1,45 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Domore.Logs.Service {
     internal sealed class ConsoleLog : ILogService {
+        private static Dictionary<LogSeverity, ConsoleColor> ForegroundDefault => new Dictionary<LogSeverity, ConsoleColor> {
+            { LogSeverity.Debug,    ConsoleColor.Cyan },
+            { LogSeverity.Info,     ConsoleColor.Gray },
+            { LogSeverity.Warn,     ConsoleColor.Yellow },
+            { LogSeverity.Error,    ConsoleColor.Red },
+            { LogSeverity.Critical, ConsoleColor.Black }
+        };
+
+        private static Dictionary<LogSeverity, ConsoleColor> BackgroundDefault => new Dictionary<LogSeverity, ConsoleColor> {
+            { LogSeverity.Debug,    ConsoleColor.Black },
+            { LogSeverity.Info,     ConsoleColor.Black },
+            { LogSeverity.Warn,     ConsoleColor.Black },
+            { LogSeverity.Error,    ConsoleColor.Black },
+            { LogSeverity.Critical, ConsoleColor.White }
+        };
+
+        public Dictionary<LogSeverity, ConsoleColor> Foreground {
+            get => _Foreground ?? (_Foreground = ForegroundDefault);
+            set => _Foreground = value;
+        }
+        private Dictionary<LogSeverity, ConsoleColor> _Foreground;
+
+        public Dictionary<LogSeverity, ConsoleColor> Background {
+            get => _Background ?? (_Background = BackgroundDefault);
+            set => _Background = value;
+        }
+        private Dictionary<LogSeverity, ConsoleColor> _Background;
+
+
         public void Log(string name, string data, LogSeverity severity) {
+            var prevForeground = Console.ForegroundColor;
+            var prevBackground = Console.BackgroundColor;
+            Console.ForegroundColor = Foreground.TryGetValue(severity, out var foreground) ? foreground : prevForeground;
+            Console.BackgroundColor = Background.TryGetValue(severity, out var background) ? background : prevBackground;
             Console.WriteLine(data);
+            Console.ForegroundColor = prevForeground;
+            Console.BackgroundColor = prevBackground;
         }
     }
 }
