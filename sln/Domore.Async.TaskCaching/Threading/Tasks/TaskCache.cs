a1d9bfb449efeccda8673eb3b1eae5cc76e69d4e
Ken Yourek <ken@yourek.net>
2024-07-05 08:51:47 -0500

    Add XML docs and write tests.

--- sln/Domore.Async.TaskCaching/Threading/Tasks/TaskCache.cs
+++ sln/Domore.Async.TaskCaching/Threading/Tasks/TaskCache.cs
@@ -3,19 +3,47 @@ using System.Threading;
 using System.Threading.Tasks;
 
 namespace Domore.Threading.Tasks {
+    /// <summary>
+    /// Caches the result of a <see cref="System.Threading.Tasks.Task"/> upon its first successful completion.
+    /// </summary>
+    /// <typeparam name="TResult">The type of the result of the task.</typeparam>
     public class TaskCache<TResult> {
         private readonly object Locker = new();
 
         private bool Cached;
         private volatile Task<TResult> Task;
 
+        /// <summary>
+        /// Gets the value of the result of the task that was cached due to successful completion of the task,
+        /// or the default value of <see cref="TResult"/> if the task has not yet successfully completed.
+        /// </summary>
         public TResult Result { get; private set; }
+
+        /// <summary>
+        /// Gets the callback used to create the instance of <see cref="System.Threading.Tasks.Task"/> whose
+        /// result is cached.
+        /// </summary>
         public Func<CancellationToken, Task<TResult>> Factory { get; }
 
+        /// <summary>
+        /// Creates a new instance of a task cache.
+        /// </summary>
+        /// <param name="factory">The callback used to create the instance of <see cref="System.Threading.Tasks.Task"/> whose result is cached.</param>
+        /// <exception cref="ArgumentNullException">Thrown if <paramref name="factory"/> is null.</exception>
         public TaskCache(Func<CancellationToken, Task<TResult>> factory) {
             Factory = factory ?? throw new ArgumentNullException(nameof(factory));
         }
 
+        /// <summary>
+        /// Returns the result of the task returned from the <see cref="Factory"/> callback upon its first successful completion.
+        /// </summary>
+        /// <param name="token">The cancellation token passed to the <see cref="Factory"/> callback.</param>
+        /// <returns>
+        /// The cached instance of <see cref="System.Threading.Tasks.Task"/> that was the result of the first successful completion 
+        /// of the task returned from the <see cref="Factory"/> callback, or the most recent instance returned from the callback if
+        /// the task completes with a fault or cancellation.
+        /// </returns>
+        /// <exception cref="InvalidOperationException">Thrown if the <see cref="Factory"/> callback returns null.</exception>
         public async Task<TResult> Ready(CancellationToken token) {
             if (Cached == false) {
                 if (Task == null) {
@@ -39,15 +67,34 @@ namespace Domore.Threading.Tasks {
             return Result;
         }
 
+        /// <summary>
+        /// An implementation of <see cref="TaskCache{TResult}"/> that allows resetting state.
+        /// </summary>
         public class WithRefresh : TaskCache<TResult> {
+            /// <summary>
+            /// Creates a new instance of a <see cref="TaskCache{TResult}"/> that allows resetting state.
+            /// </summary>
+            /// <param name="factory">The callback used to create the instance of <see cref="System.Threading.Tasks.Task"/> whose result is cached.</param>
             public WithRefresh(Func<CancellationToken, Task<TResult>> factory) : base(factory) {
             }
 
+            /// <summary>
+            /// Returns a task that completes after the cache is refreshed and the task returned from the <see cref="Factory"/> callback completes.
+            /// </summary>
+            /// <param name="token">The cancellation token passed to the <see cref="Factory"/> callback.</param>
+            /// <returns>
+            /// The cached instance of <see cref="System.Threading.Tasks.Task"/> that was the result of the first successful completion 
+            /// of the task returned from the <see cref="Factory"/> callback, or the most recent instance returned from the callback if
+            /// the task completes with a fault or cancellation.
+            /// </returns>
             public Task<TResult> Refreshed(CancellationToken token) {
                 Refresh();
                 return Ready(token);
             }
 
+            /// <summary>
+            /// Clears the cache, meaning the next call to <see cref="Ready"/> or <see cref="Refreshed"/> will call the <see cref="Factory"/> callback again.
+            /// </summary>
             public void Refresh() {
                 lock (Locker) {
                     Task = null;
--- sln/test/Domore.Async.TaskCaching.Tests/Threading/Tasks/TaskCacheTest.cs
+++ sln/test/Domore.Async.TaskCaching.Tests/Threading/Tasks/TaskCacheTest.cs
@@ -1,4 +1,7 @@
 ﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -17,6 +20,90 @@ namespace Domore.Threading.Tasks {
             var subject = new TaskCache<object>(async _ => await Get(expected));
             var actual = await subject.Ready(CancellationToken.None);
             Assert.That(actual, Is.SameAs(expected));
+        }
+
+        [TestCase(1)]
+        [TestCase(10)]
+        [TestCase(100)]
+        [TestCase(1000)]
+        public void Ready_ReturnsTheSameResultConcurrently(int n) {
+            var entry = 0;
+            var result = new object();
+            var subject = new TaskCache<object>(async _ => {
+                return 0 == entry++
+                    ? await Get(result)
+                    : null;
+            });
+            var results = new List<object>();
+            Enumerable.Range(0, n).ToList().ForEach(_ => ThreadPool.QueueUserWorkItem(async _ => {
+                var item = await subject.Ready(CancellationToken.None);
+                lock (results) {
+                    results.Add(item);
+                }
+            }));
+            while (results.Count < n) {
+                Thread.Sleep(0);
+            }
+            var expected = Enumerable.Range(0, n).Select(_ => result);
+            var actual = results;
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public async Task Ready_ThrowsTheExceptionThrownFromFactory() {
+            var expected = new Exception();
+            var actual = default(Exception);
+            var subject = new TaskCache<object>(_ => throw expected);
+            var task = subject.Ready(CancellationToken.None);
+            try {
+                await task;
+            }
+            catch (Exception ex) {
+                actual = ex;
+            }
+            Assert.That(actual, Is.SameAs(expected));
+        }
+
+        [Test]
+        public async Task Ready_ThrowsMostRecentExceptionThrownFromFactory() {
+            var n = 1;
+            var ex1 = new ArgumentException();
+            var ex2 = new InvalidOperationException();
+            var actual = new List<Exception>();
+            var subject = new TaskCache<object>(_ => throw (1 == n++ ? ex1 : ex2));
+            try {
+                await subject.Ready(CancellationToken.None);
+            }
+            catch (Exception ex) {
+                actual.Add(ex);
+            }
+            try {
+                await subject.Ready(CancellationToken.None);
+            }
+            catch (Exception ex) {
+                actual.Add(ex);
+            }
+            var expected = new Exception[] { ex1, ex2 };
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public async Task Ready_ThrowsExceptionWhenCanceled() {
+            using (var tokenSource = new CancellationTokenSource()) {
+                var actual = default(OperationCanceledException);
+                var subject = new TaskCache<object>(async token => {
+                    token.ThrowIfCancellationRequested();
+                    return await Get(new object());
+                });
+                tokenSource.Cancel();
+                try {
+                    await subject.Ready(tokenSource.Token);
+                }
+                catch (OperationCanceledException ex) {
+                    actual = ex;
+                }
+                Assert.That(actual.CancellationToken, Is.EqualTo(tokenSource.Token));
+            }
         }
     }
 }
