22551ecf012fe784ce53560c12b6d056c0c02901
Ken Yourek <ken@yourek.net>
2025-01-15 09:35:36 -0600

    Specify user name and email for git commits.

--- sln/Domore.Release.Core/ReleaseActions/Tag.cs
+++ sln/Domore.Release.Core/ReleaseActions/Tag.cs
@@ -1,8 +1,18 @@
 ﻿namespace Domore.ReleaseActions {
     internal class Tag : ReleaseAction {
         public string Stage { get; set; }
+        public string UserName { get; set; }
+        public string UserEmail { get; set; }
 
         public override void Work() {
+            var userName = UserName;
+            if (userName != null) {
+                Process("git", "config", "user.name", $"\"{userName}\"");
+            }
+            var userEmail = UserEmail;
+            if (userEmail != null) {
+                Process("git", "config", "user.email", $"\"{userEmail}\"");
+            }
             var tag = Solution.GetVersion(Stage).StagedVersion;
             Process("git", "commit", "-a", "-m", $"\"(Auto-)Commit version '{tag}'.\"");
             Process("git", "push");
