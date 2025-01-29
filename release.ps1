fa64133b87433ed8f1c89c69a7ca87b416680b92
KenY <ken@yourek.net>
2025-01-29 12:41:48 -0600

    Update release commands.

--- release.ps1
+++ release.ps1
@@ -1,8 +1,7 @@
-dotnet restore
-
-dotnet build --no-restore --configuration Release
-
-dotnet test --no-restore --configuration Release --no-build --verbosity normal
-
-dotnet pack --no-restore --configuration Release --no-build --output ./pack-output
-
+#dotnet restore
+#dotnet build --no-restore --configuration Release   
+#dotnet test --no-restore --configuration Release --no-build --verbosity normal
+#dotnet pack --no-restore --configuration Release --no-build --output ./pack-output
+#foreach($file in (Get-ChildItem "${{ env.RAPTOR_PACK_OUTPUT }}" -Recurse -Include *.nupkg)) {
+#  dotnet nuget push $file --api-key "${{ secrets.GITHUB_TOKEN }}" --source https://nuget.pkg.github.com/peddinghaus/index.json --skip-duplicate
+#}
