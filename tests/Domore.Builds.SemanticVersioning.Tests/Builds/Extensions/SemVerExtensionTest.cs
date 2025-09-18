63ead0f46b98f01b5beca189c65f8ab8c3d38cc2
KenY <ken@yourek.net>
2025-09-18 11:22:49 -0500

    Remove unused SemVer methods.

--- tests/Domore.Builds.SemanticVersioning.Tests/Builds/Extensions/SemVerExtensionTest.cs
+++ tests/Domore.Builds.SemanticVersioning.Tests/Builds/Extensions/SemVerExtensionTest.cs
@@ -11,4 +11,44 @@ internal sealed class SemVerExtensionTest {
         var expected = "alpha.5";
         Assert.That(actual, Is.EqualTo(expected));
     }
+
+    [Test]
+    public void VersionPrefix_ExcludesPreRelease() {
+        var semVer = new SemVer(2, 3, 4, "alpha.5");
+        var actual = SemVerExtension.VersionPrefix(semVer);
+        var expected = "2.3.4";
+        Assert.That(actual, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void AssemblyVersion_ExcludesPreRelease() {
+        var semVer = new SemVer(2, 3, 4, "alpha.5");
+        var actual = SemVerExtension.AssemblyVersion(semVer);
+        var expected = "2.3.4";
+        Assert.That(actual, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void FileVersion_ExcludesPreRelease() {
+        var semVer = new SemVer(2, 3, 4, "alpha.5");
+        var actual = SemVerExtension.FileVersion(semVer);
+        var expected = "2.3.4";
+        Assert.That(actual, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void InformationalVersion_IncludesPreRelease() {
+        var semVer = new SemVer(2, 3, 4, "alpha.5");
+        var actual = SemVerExtension.InformationalVersion(semVer);
+        var expected = "2.3.4-alpha.5";
+        Assert.That(actual, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void PackageVersion_IncludesPreRelease() {
+        var semVer = new SemVer(2, 3, 4, "alpha.5");
+        var actual = SemVerExtension.PackageVersion(semVer);
+        var expected = "2.3.4-alpha.5";
+        Assert.That(actual, Is.EqualTo(expected));
+    }
 }
